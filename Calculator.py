281a4af7b7056560755c8ff9cac209d487d4908a
matthewkeaton5 <56092226+matthewkeaton5@users.noreply.github.com>
2020-07-17 21:25:17 -0400

    Added: functions to Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+def add(int1,int2):
+    return int1 + int2
+def sub(int1,int2):
+    return int1 - int2
+def mult(int1,int2):
+    return int1 * int2
+def div(int1,int2):
+    return int1 / int2
+def square(int1):
+    return int1**2
+def root(int1):
+    return int1**(1/2)
+
+
